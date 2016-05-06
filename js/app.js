54f4af7c80c4166844d71a06bdf25d6957998cdc
Krystyna Singleton <ksingleton88@gmail.com>
2016-05-05 18:18:38 -0700

    Finished Game

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
+
 $(document).ready(function(){
+	var counter = 0;
+	var secretNumber;
+
+	var button = document.getElementById('guessButton');
+	var display = document.getElementById('count');
 
 //New Game - Clears Counter
 	function newGame(){
-		var secretNumber = Math.floor(Math.random()*100) + 1; //Gernerates #
-		// console.log('secretNumber', secretNumber); shows secret number in console.
+		secretNumber = Math.floor(Math.random()*100) + 1; //Gernerates #
+		//console.log('secretNumber', secretNumber); shows secret number in console.
 
 		counter = 0;
 		display.innerHTML = counter;
 
 		$('.guessBox').html(''); //resets html and results
+
+		document.getElementById('feedback').innerHTML = "Make Your Guess!";
 
 	}
 
@@ -19,10 +27,6 @@ $(document).ready(function(){
 	}
 
 // COUNTER FOR AMOUNT OF GUESSES
-	var counter = 0;
-
-	var button = document.getElementById('guessButton');
-	var display = document.getElementById('count');
 
 	button.onclick = function(){
 		counter ++;
@@ -32,11 +36,37 @@ $(document).ready(function(){
 // LISTS GUESSED NUMBERS
 	$('form').submit(function (e) {
 		e.preventDefault();
-		var toAdd = $("input[name=userGuess]").val();
-		$(".guessBox").append("<li>" + " " + toAdd + "</li>");
+		var guess = $("input[name=userGuess]").val();
+		guessNumber(guess);
+		$(".guessBox").append("<li>" + " " + guess + "</li>");
 		$('#userGuess').val('');
 	});
-	
+
+//check guessed number
+
+	function guessNumber(number){
+		var diff = Math.abs(secretNumber - number); 
+		
+		if(secretNumber == number){
+			document.getElementById('feedback').innerHTML = "You Win!";
+		}
+		else if(diff <= 5){
+			document.getElementById('feedback').innerHTML = "FIRE!";
+		}
+		else if(diff <= 10){
+			document.getElementById('feedback').innerHTML = "Getting Warmer!";
+		}
+		else if(diff <= 20){
+			document.getElementById('feedback').innerHTML = "Cold";
+		}
+		else if(diff <= 50){
+			document.getElementById('feedback').innerHTML = "FREEZING!";
+		}
+		else if(diff > 50){
+			document.getElementById('feedback').innerHTML = "You are going to have to do better than that!"
+		}
+	}
+//supply feedback
 
 //Click New Game 
 	$('.new').click(function(){
